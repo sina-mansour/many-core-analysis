4c22e4dc77a04a519d71b9699767afaf1453624b
Sina Mansour L <sina.mansour.lakourag@gmail.com>
2016-07-13 12:27:38 +0430

    updated sdf transform code

--- transform/transform.py
+++ transform/transform.py
@@ -6,38 +6,46 @@ import sdfparser
 import sdftransformer
 import sdfwriter
 
-### do we have an input file ###
-if (len(sys.argv) < 2):
-	print "Usage: " + sys.argv[0] + " <graph_file>"
-	exit(0)
 
-print "Hello"
-print "Transforms SDF3 SDF XML file"
-print "Make all actors to do fine-grained reading and writing in sequential order to channels"
+def transform(fileName):
+	### open file to read ###
+	print ("Loading ---> " + fileName)
+	f = open(fileName, "r")
 
-### sobreity check ###
-if (sys.hexversion < 33948912):
-	print "WARNING: This program was designed using Python 2.6.4"
-	print "WARNING: You are running:\nPython: " + sys.version
-	print "It could still work..."
+	### setup the parser ###
+	parser = xml.parsers.expat.ParserCreate()
+	parser.StartElementHandler = sdfparser.start_element
+	parser.EndElementHandler = sdfparser.end_element
 
-### open file to read ###
-print "Loading ---> " + sys.argv[1]
-f = open(sys.argv[1], "r")
+	print ("Parsing")
+	parser.ParseFile(f)
+	f.close()
 
-### setup the parser ###
-parser = xml.parsers.expat.ParserCreate()
-parser.StartElementHandler = sdfparser.start_element
-parser.EndElementHandler = sdfparser.end_element
+	print ("Transforming")
+	(actors, actor_times, channels, channel_sizes)=sdftransformer.transform(sdfparser.actors, sdfparser.actor_times, sdfparser.channels, sdfparser.channel_sizes)
 
-print "Parsing"
-parser.ParseFile(f)
-f.close()
+	print ("Writing output file ---> transformed_" + fileName)
+	sdfwriter.write_graph_to_file("transformed_"+fileName, actors, actor_times, channels, channel_sizes)
 
-print "Transforming"
-(actors, actor_times, channels, channel_sizes)=sdftransformer.transform(sdfparser.actors, sdfparser.actor_times, sdfparser.channels, sdfparser.channel_sizes)
+	print ("Success")
 
-print "Writing output file ---> transformed_" + sys.argv[1]
-sdfwriter.write_graph_to_file("transformed_"+sys.argv[1], actors, actor_times, channels, channel_sizes)
+def main():
+	### do we have an input file ###
+	if (len(sys.argv) < 2):
+		print ("Usage: " + sys.argv[0] + " <graph_file>")
+		sys.exit(0)
 
-print "Success"
+	print ("Hello")
+	print ("Transforms SDF3 SDF XML file")
+	print ("Make all actors to do fine-grained reading and writing in sequential order to channels")
+
+	### sobreity check ###
+	if (sys.hexversion < 34014960):
+		print ("WARNING: This program was designed using Python 2.7.6")
+		print ("WARNING: You are running:\nPython: " + sys.version)
+		print ("It could still work...")
+
+	transform(sys.argv[1])
+
+
+if __name__ == '__main__': main()
